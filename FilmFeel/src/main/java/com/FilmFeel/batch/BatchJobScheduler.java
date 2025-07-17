25b7496335f2276847fa067f20bf7d84710a58c4
AleNa <alenaveirabarbeito@gmail.com>
2025-07-17 12:49:35 +0200

    cambios 1249

---
+++ FilmFeel/src/main/java/com/FilmFeel/batch/BatchJobScheduler.java
@@ -0,0 +1,29 @@
+package com.FilmFeel.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class BatchJobScheduler {
+
+    private final JobLauncher jobLauncher;
+    private final Job filmMigrationJob;
+
+    @Scheduled(cron = "0 */5 * * * *")  // cada día a medianoche
+    public void launchJob() throws Exception {
+        String fileName = LocalDate.now() + "_films.csv";
+        var params = new JobParametersBuilder()
+                .addString("output.file.name", fileName)
+                .addDate("runDate", new Date())
+                .toJobParameters();
+        jobLauncher.run(filmMigrationJob, params);
+    }
+}
